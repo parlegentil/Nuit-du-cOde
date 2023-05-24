73e1b131352acf688c536cd62c23deb6b9ac3ded
parlegentil <112904455+parlegentil@users.noreply.github.com>
2023-05-24 13:25:08 +0200

    Update vaisseau.py

--- vaisseau.py
+++ vaisseau.py
@@ -3,68 +3,48 @@ class vaisseau:
 
     def __init__(self, coord):
         self.coord = coord
+        self.directions = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)]
     
     def sur_ennemi(self):
         pass
 
     def pos_dans_lab(self, lab):
         position = (0, 0)
-        for i in range(lab.coord):
-            for j in range(lab.coord[0]):
-                if self.coord == self.lab[i][j]:
+        for i in range(len(lab.coords)):
+            for j in range(len(lab.coords[0])):
+                if self.coord == lab.coords[i][j]:
                     position = (i, j)
         return position
 
+    #
+    def presence_sol(self, dire, lab):
 
-    def presence_mur(self, dir, lab):
-        return lab[self.pos_dans_lab()[0] + dir[0]][self.pos_dans_lab()[1] + dir[1]] == 1
-    
-    def mouvement(self, dir, lab):
-        
-
+        return lab.murs[self.pos_dans_lab(lab)[0] + self.directions[dire][0]][self.pos_dans_lab(lab)[1] + self.directions[dire][1]] == 1
     
 
-    def dessin(self, dir):
+    def mouvement(self, dire, lab):
         
-        if dir == 4:
+
+        if not self.presence_sol(dire, lab):
+            self.coord = (self.coord[0] + self.directions[dire][0], self.coord[1] + self.directions[dire][1])
+
+
+    def dessin(self, dire, lab):
+        
+        self.mouvement(dire, lab)
+
+        if dire == 4:
             pyxel.blt(self.coord[0], self.coord[1], 0, 2, 1, 3, 6)
             pyxel.blt(self.coord[0] + 3, self.coord[1], 0, 2, 1, -3, 6)
 
-        if dir == 3:
+        if dire == 3:
             pyxel.blt(self.coord[0], self.coord[1], 0, 2, 1, 3, -6)
             pyxel.blt(self.coord[0] + 3, self.coord[1], 0, 2, 1, -3, -6)
 
-        if dir == 1:
+        if dire == 1:
             pyxel.blt(self.coord[0], self.coord[1], 0, 77, 41, 6, 3)
             pyxel.blt(self.coord[0], self.coord[1] + 3, 0, 77, 41, 6, -3)
 
-        if dir == 2:
+        if dire == 2:
             pyxel.blt(self.coord[0], self.coord[1], 0, 77, 41, -6, 3)
             pyxel.blt(self.coord[0], self.coord[1] + 3, 0, 77, 41, -6, -3)
-
-
-
-
-
-class Jeu():
-
-    directions = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)]
-
-    def __init__(self):
-        self.lab = Labyrinthe()
-        self.vso = vaisseau()
-
-    def draw(self):
-        pass
-    
-    def update(self):
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            dir = 1
-        elif pyxel.btn(pyxel.KEY_LEFT):
-            dir = 2
-        elif pyxel.btn(pyxel.KEY_UP):
-            dir = 4
-        elif pyxel.btn(pyxel.KEY_DOWN):
-            dir = 3
-        else:
-            dir = 0
