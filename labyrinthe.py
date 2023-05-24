c0694560c70268bf54ceda6bbd01026ccab63bf2
Maxime <maxime.langlet@hotmail.com>
2023-05-24 11:53:27 +0200

    Add files via upload

---
+++ labyrinthe.py
@@ -0,0 +1,69 @@
+import pyxel
+import pprint
+
+class Labyrinthe:
+    """"""
+
+    def __init__(self):
+        pyxel.init(128, 128)
+        self.origine = (1, 1)
+        pyxel.mouse(visible=True)
+
+        pyxel.load("3.pyxres")
+
+        self.nb_blocs = 21
+        self.taille_bloc = 6
+
+        self.murs = [[1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1],
+                     [1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+                     [0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+                     [1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+                     [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+                     [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+                     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+                     [1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+                     [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+                     [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+                     [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+                     [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+                     [1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+                     [0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
+                     [1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0],
+                     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1],
+                     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0],
+                     [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1],
+                     [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+                     [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0],
+                     [1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]]
+
+        self.coords= [[[self.origine[0] + (ligne * self.taille_bloc),
+                        self.origine[1] + (colonne * self.taille_bloc)]
+                       for ligne in range(self.nb_blocs)]
+                      for colonne in range(self.nb_blocs)]
+
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+
+    def draw(self):
+        pyxel.cls(0)
+
+        for ligne in range(self.nb_blocs):
+            for colonne in range(self.nb_blocs):
+                bloc = self.coords[ligne][colonne]
+                murs = self.murs
+                if murs[ligne][colonne] == 1:
+                    x = bloc[0]
+                    y = bloc[1]
+                    pyxel.blt(x, y, 0, 225, 40, 6, 6)
+
+lab = Labyrinthe()
+
+# pp = pprint.PrettyPrinter()
+# pp.pprint(lab.matrice)
+
+pyxel.run(lab.update, lab.draw)
+
+
